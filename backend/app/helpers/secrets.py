7a0e3dc281054896739a254f571cd9c2e8449c7e
Ricardo <rvaken98@gmail.com>
2024-03-11 21:09:44 +0100

    Added secrets, but can't check if it works when not on lambda

---
+++ backend/app/helpers/secrets.py
@@ -0,0 +1,28 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+    secret_name = "MongoDB"
+    region_name = "eu-north-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    username = get_secret_value_response['Username']
+    password = get_secret_value_response['Password']
+
+    return username, password
